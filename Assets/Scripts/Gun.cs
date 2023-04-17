cc8007522858ef1b4466f2e902852600cf524adc
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-04-17 14:33:38 -0700

    got bullet instaiating and direction
    need to make public event to move bullet * time.deltTime

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,74 +2,91 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Gun : MonoBehaviour
 {
 
-    [Header("References")]
-    [SerializeField] private GunData gunData;
-    [SerializeField] private Transform cam;
+	[Header("References")]
+	[SerializeField] private GunData gunData;
+	[SerializeField] private Transform cam;
+	public Transform Muzzle;
+	public GameObject Bullet;
+	public int bulletSpeed;
 
-    float timeSinceLastShot;
+	float timeSinceLastShot;
 
-    private void Start()
-    {
-        PlayerShoot.shootInput += Shoot;
-        PlayerShoot.reloadInput += StartReload;
-    }
+	private void Start()
+	{
+		PlayerShoot.shootInput += Shoot;
+		PlayerShoot.reloadInput += StartReload;
+	}
 
-    private void OnDisable() => gunData.reloading = false;
+	private void OnDisable() => gunData.reloading = false;
 
-    public void StartReload()
-    {
-        if (!gunData.reloading && this.gameObject.activeSelf)
-            StartCoroutine(Reload());
-    }
+	public void StartReload()
+	{
+		if (!gunData.reloading && this.gameObject.activeSelf)
+			StartCoroutine(Reload());
+	}
 
-    private IEnumerator Reload()
-    {
-        gunData.reloading = true;
+	private IEnumerator Reload()
+	{
+		gunData.reloading = true;
 
-        yield return new WaitForSeconds(gunData.reloadTime);
+		yield return new WaitForSeconds(gunData.reloadTime);
 
-        gunData.currentAmmo = gunData.magSize;
+		gunData.currentAmmo = gunData.magSize;
 
-        gunData.reloading = false;
-    }
+		gunData.reloading = false;
+	}
 
-    private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
+	private bool CanShoot() => !gunData.reloading && timeSinceLastShot > 1f / (gunData.fireRate / 60f);
 
-    private void Shoot()
-    {
-        if (gunData.currentAmmo > 0)
-        {
-            if (CanShoot())
-            {
-                if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance))
-                {
-                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
-                    damageable?.TakeDamage(gunData.damage);
-                }
+	private void Shoot()
+	{
+		gunData.currentAmmo = 5;
+		if (gunData.currentAmmo > 0)
+		{
+			if (CanShoot())
+			{
+				var direction = cam.forward;
 
-                gunData.currentAmmo--;
-                timeSinceLastShot = 0;
-                OnGunShot();
-            }
-        }
-    }
+				direction /= direction.magnitude;
 
-    private void Update()
-    {
-        timeSinceLastShot += Time.deltaTime;
+				Debug.Log(direction);
 
-        Debug.DrawRay(cam.position, cam.forward * gunData.maxDistance);
-    }
+				GameObject newBullet = GameObject.Instantiate(Bullet, Muzzle.transform.position, Muzzle.rotation);
 
-    private void OnDestroy()
-    {
-        PlayerShoot.shootInput -= Shoot;
-        PlayerShoot.reloadInput -= StartReload;
-    }
+				//this should call en event to move the instatated bullet
+				//newBullet.transform.position += direction * bulletSpeed * Time.deltaTime;
+					
 
-    private void OnGunShot() { }
+				gunData.currentAmmo--;
+				
+				timeSinceLastShot = 0;
+				OnGunShot();
+			}
+		}
+	}
+
+	private void Update()
+	{
+		timeSinceLastShot += Time.deltaTime;
+
+		Debug.DrawRay(cam.position, cam.forward * 5);
+
+		if (Input.GetAxis("Fire1") == 1)
+		{
+			Shoot();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		PlayerShoot.shootInput -= Shoot;
+		PlayerShoot.reloadInput -= StartReload;
+	}
+
+	private void OnGunShot() { }
 }
