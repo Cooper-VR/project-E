a405d8dcaca3a6e98f4094fd5fe90f7dc239256c
sugar studios <abcalkins8723@gmail.com>
2023-04-28 14:37:25 -0700

    asdfiohw iafu shafiodjsa

--- Assets/Scripts/GameMenu/GameMenuManager.cs
+++ Assets/Scripts/GameMenu/GameMenuManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameMenuManager : MonoBehaviour
@@ -14,16 +15,93 @@ public class GameMenuManager : MonoBehaviour
     */
 
     //For the sake of testing the Player is automatically set to "new" status and the username is not saved. Both of these will be accounted for once implemented with the save system. 
-    bool new_player = true;
+    public bool new_player = true;
+    //Get the rest of variables nessisary for script.
+    public TMP_Text UsernameField;
+    public TMP_Text ProfileName;
+    public GameObject ErrorText;
+
+    public GameObject[] Menus;
 
     void Start()
     {
-        
+        try
+        {
+            ProfileName.text = "Profile";
+        }
+        catch
+        {
+            ProfileName.text = "Profile";
+        }
+        if (new_player)
+        {
+            for (int i = 0; i < Menus.Length; i++)
+            {
+                Debug.Log(Menus[i].ToString());
+                if (Menus[i].ToString() == "NewPlayer" || i == 3 || Menus[i].ToString() == "Background" || i == 0)
+                {
+                    
+                    Menus[i].SetActive(true);
+                }
+                else
+                {
+                    Menus[i].SetActive(false);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SwitchMenu(string menu)
+    {
+        for (int i = 0; i < Menus.Length; i++)
+        {
+            Debug.Log(Menus[i].ToString());
+            if (Menus[i].ToString() == "NewPlayer")
+            {
+
+                Menus[i].SetActive(true);
+            }
+            else
+            {
+                Menus[i].SetActive(false);
+            }
+        }
+    }
+
+    public void OnSubmit()
+    {
+        if (CheckValidity())
+        {
+            Debug.Log("Register Acount");
+            Menus[3].SetActive(false);
+            ProfileName.text = UsernameField.text;
+        }
+    }
+    
+
+    public bool CheckValidity()
+    {
+        //This function checks the length of the submitted username, if the username is too long retrun false and display error text.
+
+        //Get the username and turn error text off.
+        string Username = UsernameField.text;
+
+        //Check username validity return the result. 
+        if (Username.Length >= 10 || Username == null || Username.Length == 1)
+        {
+            ErrorText.SetActive(true);
+            return false;
+        }
+        else
+        {
+            ErrorText.SetActive(false);
+            return true;
+        }
     }
 }
