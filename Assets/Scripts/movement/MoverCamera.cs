cc335f9d8e76b0971924fa2cb07289bcbe8837d4
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-05-02 13:11:51 -0700

    movement anims done, camera position fixed
    need gun stuff

--- Assets/Scripts/movement/MoverCamera.cs
+++ Assets/Scripts/movement/MoverCamera.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoverCamera : MonoBehaviour
 {
     [SerializeField] Transform cameraPosition = null;
     [SerializeField] FirstPersonController controller;
-    float currentY;
-    float currentVelocity;
+    public float offset;
+
+    float difference;
+
+    public Transform head;
+
+    private void Start()
+    {
+        difference = head.transform.position.y - cameraPosition.position.y;
+    }
 
     void Update()
     {
-        transform.position = new Vector3(cameraPosition.position.x, cameraPosition.position.y + .9f, cameraPosition.position.z);
+        transform.position = new Vector3(cameraPosition.position.x, cameraPosition.position.y + difference, cameraPosition.position.z);
     }
 }
--- Assets/playerAnimation.cs
+++ Assets/playerAnimation.cs
@@ -7,12 +7,18 @@ public class playerAnimation : MonoBehaviour
 {
 	public KeyCode[] movement = new KeyCode[4];
 	public Animator animator;
+	public float transitionMultiplier;
+	public Transform playerBody;
 
 	private int forward;
 	private int sideways;
+	private Vector3 playerTransform;
+    private float YTime = 0;
+    private float XTime = 0;
 
-	private void Start()
+    private void Start()
 	{
+		playerTransform = playerBody.transform.localPosition;
 		movement[0] = KeyCode.W;
 		movement[1] = KeyCode.S;
 		movement[2] = KeyCode.D;
@@ -21,14 +27,16 @@ public class playerAnimation : MonoBehaviour
 
 	private void Update()
 	{
+		playerBody.transform.localPosition = playerTransform;
+
 		if (Input.GetKey(movement[0]) && Input.GetKeyDown(movement[1]))
 		{
 			forward = 0;
 		}
 		else if (Input.GetKey(movement[0]))
 		{
-            Debug.Log(forward);
-            forward = 1;
+			Debug.Log(forward);
+			forward = 1;
 		} 
 		else if (Input.GetKey(movement[1]))
 		{
@@ -40,28 +48,39 @@ public class playerAnimation : MonoBehaviour
 		}
 
 
-        if (Input.GetKey(movement[2]) && Input.GetKeyDown(movement[3]))
-        {
-            sideways = 0;
-        }
-        else if (Input.GetKey(movement[2]))
-        {
-            Debug.Log(sideways);
-            sideways = 1;
-        }
-        else if (Input.GetKey(movement[3]))
-        {
-            sideways = -1;
-        }
-        else
-        {
-            sideways = 0;
-        }
+		if (Input.GetKey(movement[2]) && Input.GetKeyDown(movement[3]))
+		{
+			sideways = 0;
+		}
+		else if (Input.GetKey(movement[2]))
+		{
+			Debug.Log(sideways);
+			sideways = 1;
+		}
+		else if (Input.GetKey(movement[3]))
+		{
+			sideways = -1;
+		}
+		else
+		{
+			sideways = 0;
+		}
 
-      
 
-		animator.SetFloat("forward", forward);
-        animator.SetFloat("sideWays", sideways);
+		float xDifference = sideways - animator.GetFloat("sideWays");
+        float yDifference = forward - animator.GetFloat("forward");
+
+        XTime += xDifference * Time.deltaTime * transitionMultiplier;
+		YTime += yDifference * Time.deltaTime * transitionMultiplier;
+
+        XTime = Mathf.Clamp(XTime, -1, 1);
+        YTime = Mathf.Clamp(YTime, -1, 1);
+
+        animator.SetFloat("sideWays", XTime);
+        animator.SetFloat("forward", YTime);
+
+
+
 
     }
 }
