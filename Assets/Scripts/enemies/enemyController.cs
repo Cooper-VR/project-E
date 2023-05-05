6dad62155bff5749ef9a81284f2e1cbcce056f98
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-05-05 14:34:35 -0700

    fixing creeper explode, reload done

--- Assets/Scripts/enemies/enemyController.cs
+++ Assets/Scripts/enemies/enemyController.cs
@@ -122,10 +122,11 @@ public class enemyController: MonoBehaviour
             shootProjectile();
         }
     }
-	private async void creeperSet()
+	private void creeperSet()
 	{
+		
 		agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
-		if ((transform.position - GameObject.FindGameObjectWithTag("Player").transform.position).magnitude < enemyData.explotionProximity)
+		if ((transform.position - agent.destination).magnitude < enemyData.explotionProximity)
 		{
 			startTimeSet = true;
 		}
