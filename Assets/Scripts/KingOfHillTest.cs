2ea3a4fdf4444b41985d5b87b2bb265ac6c9bda2
SandwichBum <111802848+SandwichBum@users.noreply.github.com>
2023-05-12 12:41:05 -0700

    KingHill Mode

--- Assets/Scripts/KingOfHillTest.cs
+++ Assets/Scripts/KingOfHillTest.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
 
 public class KingOfHillTest : MonoBehaviour
 {
@@ -8,9 +11,12 @@ public class KingOfHillTest : MonoBehaviour
     public GameObject Hill;
     private Renderer HillColor;
     public GameObject player;
-    private float currentTime = 180f; 
-    public bool captured = false;
-    public bool blueTeam = false;
+    private float currentTime = 180f;
+    public TMP_Text timeText;
+    public bool captured= false;
+    public float speed = 0.1f;
+    public bool gameOver = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +27,19 @@ public class KingOfHillTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         
+        
+
+        if (captured)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            Debug.Log(currentTime);
+        }
+        else
+        {
+            currentTime = 180f;
+            Debug.Log(currentTime);
+        }
     }
 
     private void OnTriggerEnter(Collider Trigger)
@@ -34,17 +51,16 @@ public class KingOfHillTest : MonoBehaviour
             HillColor = Hill.GetComponent<Renderer>();
             HillColor.material.color = Color.red;
             captured = true;
+
         }
         else if (Trigger.CompareTag("Player"))
         {
             HillColor = Hill.GetComponent<Renderer>();
             HillColor.material.color = Color.blue;
-            currentTime -= 1 * Time.deltaTime;
             captured = true;
-            blueTeam = true;
-
 
         }
+
 
 
     }
@@ -53,6 +69,14 @@ public class KingOfHillTest : MonoBehaviour
     {
         HillColor = Hill.GetComponent<Renderer>();
         HillColor.material.color = Color.gray;
+        captured = false;
+
+    }
+    void DisplayTime(float currentTime)
+    {
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        string timeText = System.TimeSpan.FromSeconds(currentTime).ToString("hh':'mm':'ss");
         
 
 
